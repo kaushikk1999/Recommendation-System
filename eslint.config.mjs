8168bf5d2705f992db679ad4e8ab9f31ada23ce6
Kaushik Karmakar <kaushikka99@gmail.com>
2026-05-25 21:56:58 +0530

    Configure ESLint to ignore playwright-report and test-results

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
-const eslintConfig = [...nextVitals, ...nextTypescript];
+const eslintConfig = [
+  {
+    ignores: [
+      ".next/",
+      "node_modules/",
+      "playwright-report/",
+      "test-results/",
+      "dist/"
+    ]
+  },
+  ...nextVitals,
+  ...nextTypescript
+];
 
 export default eslintConfig;
+
